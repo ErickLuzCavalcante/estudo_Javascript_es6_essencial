fdf96301ec0b84b67a0a4190ccaa88828a60eeda
ERICK LUZ CAVALCANTE <erickl.cavalcante@gmail.com>
2020-09-02 02:43:27 -0300

    adcionado conteudo sobre o tipo de variavel null

---
+++ null.js
@@ -0,0 +1,5 @@
+const variavelNull = null;
+
+console.log("O tipo da varivel com o valor null: ", typeof variavelNull); // o tipo retornado da de uma varivel com valor null é object
+console.log("O conteudo da variavel com o valor null: ", variavelNull);
+
